d87d1ba0bf12b5eb6f411e7f89a89766aac9b97d
Chris Morton <christopher_morton@hotmail.com>
2020-09-11 16:04:11 -0400

    missed files.  Finished up day 3

---
+++ LibraryApi/Models/Books/BookCreateRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApi.Models.Books
+{
+	public class BookCreateRequest : IValidatableObject
+	{
+		[Required]
+		[MaxLength(200)]
+		public string Title { get; set; }
+		[Required]
+		[MaxLength(100)]
+		public string Author { get; set; }
+		[Required]
+		public string Genre { get; set; }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (Title.ToLower() == "it" && Author.ToLower().Contains("king"))
+			{
+				yield return new ValidationResult("That book is too darn long!");
+			}
+		}
+	}
+
+	
+}
